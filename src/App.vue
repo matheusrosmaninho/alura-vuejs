fbf21178148bd7188d3ac20473edc6399473792b
Matheus Rosmaninho <matheusrosmano@gmail.com>
2021-11-27 15:49:13 -0300

    Cria componente com css scoped

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,18 @@
     <h1 class="centralizado">{{ titulo }}</h1>
     <ul class="lista-fotos">
       <li class="lista-fotos-item" v-for="(foto, index) in fotos" :key="index">
-        <div class="painel">
-          <h2 class="painel-titulo">{{ foto.titulo }}</h2>
-          <div class="painel-conteudo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-          </div>
-        </div>
+        <painel :foto="foto">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
+        </painel>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
 export default {
+  components: { Painel },
   data() {
     return {
       titulo: "Alurapic",
@@ -48,29 +47,5 @@ export default {
 }
 .lista-fotos .lista-fotos-item {
   display: inline-block;
-}
-.imagem-responsiva {
-  width: 100%;
-}
-/* estilo do painel */
-.painel {
-  padding: 0 auto;
-  border: solid 2px grey;
-  display: inline-block;
-  margin: 5px;
-  box-shadow: 5px 5px 10px grey;
-  width: 200px;
-  height: 100%;
-  vertical-align: top;
-  text-align: center;
-}
-
-.painel .painel-titulo {
-  text-align: center;
-  border: solid 2px;
-  background: lightblue;
-  margin: 0 0 15px 0;
-  padding: 10px;
-  text-transform: uppercase;
 }
 </style>
---
+++ src/components/shared/painel/Painel.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="painel">
+    <h2 class="painel-titulo">{{ foto.titulo }}</h2>
+    <slot class="painel-conteudo"> </slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Painel",
+  props: {
+    foto: {
+      type: Object,
+      default: {
+        url: "https://via.placeholder.com/150",
+        titulo: "Imagem não encontrada"
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* estilo do painel */
+.painel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
+
+.painel .painel-titulo {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+.imagem-responsiva {
+  width: 100%;
+}
+* {
+  box-shadow: 5px 5px 5px;
+}
+</style>
